eb1d597fba41a22cb3f83381ff54a5303f3f6f3f
gio-oig <araxamiag@gmail.com>
2021-04-17 14:30:19 +0400

    remove localhost image path

--- src/components/shared/Avatar.vue
+++ src/components/shared/Avatar.vue
@@ -1,16 +1,14 @@
 <template>
-	<!-- <img
-		v-if="image"
-		:src="
-			'https://arcane-bayou-45011.herokuapp.com/uploads/images/' + image
-		"
-		alt=""
-	/> -->
 	<img
+		v-if="image"
+		:src="'https://arcane-bayou-45011.herokuapp.com/uploads/images/' + image"
+		alt=""
+	/>
+	<!-- <img
 		v-if="image"
 		:src="`http://localhost:5000/uploads/images/${image}`"
 		alt=""
-	/>
+	/> -->
 	<img v-else src="@/assets/unknown.jpg" alt="" />
 </template>
 
