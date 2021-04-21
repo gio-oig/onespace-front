0c868c033f3c97773cd3837cb9543182de421b8b
gio-oig <araxamiag@gmail.com>
2021-04-21 12:09:31 +0400

    changed socket endpoint

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,3 +1,7 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:5000');
+const ENDPOINT = process.env.NODE_ENV === 'production'
+    ? 'https://arcane-bayou-45011.herokuapp.com'
+    : 'http://localhost:5000',
+
+export const socket = io(ENDPOINT);
